b2daf6f5cd79eb4cb47316c0f4296097e5b1c552
Tsyren Ochirov <nsu1team@gmail.com>
2015-04-09 15:05:13 +0200

    Using correct name for method.

--- lib/seoshop-api/api/shop_tracking.rb
+++ lib/seoshop-api/api/shop_tracking.rb
@@ -5,6 +5,7 @@ module Seoshop
       response = get("#{@shop_language}/shop/tracking/count.json", params)
       response.body ? response.body['count'] : false
     end
+
     def get_trackings(params = {})
       response = get("#{@shop_language}/shop/tracking.json", params)
       response.body ? response.body['shopTracking'] : false
@@ -24,8 +25,10 @@ module Seoshop
       response = get("#{@shop_language}/shop/tracking/#{tracking_id}.json")
       response.body ? response.body['shopTracking'] : false
     end
+
+    # Always true? wtf?
     def delete_tracking(tracking_id)
-      response = delete("#{@shop_language}/shop/tracking/#{script_id}.json")
+      response = delete("#{@shop_language}/shop/tracking/#{tracking_id}.json")
       true
     end
   end
