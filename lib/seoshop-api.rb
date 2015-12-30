9914758430a33fadff960734ad27411fa74806d0
Tsyren Ochirov <nsu1team@gmail.com>
2015-12-30 13:51:48 +0100

    Droped the method missing overwritten method.

--- lib/seoshop-api.rb
+++ lib/seoshop-api.rb
@@ -28,31 +28,10 @@ module Seoshop
     end
 
     #
-    # Makes sure that the method missing is checked with the Seoshop::Client instance
-    #
-    # @param method_name [String] the name of the method we want to run
-    # @param include_private [Boolean] defines wether to check for private functions as well
-    def respond_to_missing?(method_name, include_private=false)
-      client.respond_to?(method_name, include_private)
-    end
-
-    #
     # @return an instance of Seoshop::Client
     #
     def client(access_token, shop_language)
       @client ||= Seoshop::Client.new(access_token, shop_language)
-    end
-
-    private
-
-    #
-    # executes any function on the Seoshop::Client instance
-    #
-    # @param args [*] any argument that we want to pass to the client function
-    # @param block [Block] any block that is passed to the client function
-    def method_missing(method_name, *args, &block)
-      return super unless client.respond_to?(method_name)
-      client.send(method_name, *args, &block)
     end
   end
 end
